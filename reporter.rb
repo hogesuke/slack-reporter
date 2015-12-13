05902730aad9d4f116a14a736c1c6c98935a8cdb
hogesuke <miyado@gmail.com>
2015-12-13 14:45:47 +0900

    チャンネルを出力するように修正

--- reporter.rb
+++ reporter.rb
@@ -3,6 +3,7 @@ require 'net/http'
 require 'uri'
 require 'erb'
 require 'date'
+require 'pp'
 
 class Reporter
 
@@ -12,20 +13,26 @@ class Reporter
 
   def run
     channels = fetch_channels
-    histories = fetch_history(channels)
+
+    histories = {}
+    channels.each do |c|
+      histories[c['id']] = fetch_history(c)
+    end
 
     # histories.each do |h|
     #   puts h['messages']
     # end
 
-    user_ids = histories.map do |h|
-      h['messages'].map{ |m| m['user'] }
+    user_ids = []
+    histories.each do |ch_id, history|
+      user_ids << history['messages'].select{ |m| !m['user'].nil? }.map{ |m| m['user'] }
     end
     user_ids.flatten!.uniq!
+    pp user_ids
 
     users = fetch_users(user_ids)
     user_names = {}
-    users.map{ |u| p u; p user_names; user_names[u['id']] = u['name'] }
+    users.map{ |u| user_names[u['id']] = u['name'] }
 
     template = File.read(File.join(__dir__, './templates/main.erb'))
     html = ERB.new(template).result(binding)
@@ -54,17 +61,13 @@ class Reporter
     users.select{ |u| !u['user'].nil? }.map{ |u| u['user'] }
   end
 
-  def fetch_history(channels)
-    histories = []
-    channels.each do |c|
-      url = URI.parse("https://slack.com/api/channels.history?token=#{@token}&channel=#{c['id']}&count=2")
-      puts 'start history request'
-      res = Net::HTTP.get(url)
-      puts 'end history request'
-      histories << JSON.parse(res)
-    end
+  def fetch_history(channel)
+    url = URI.parse("https://slack.com/api/channels.history?token=#{@token}&channel=#{channel['id']}&count=2")
+    puts 'start history request'
+    res = Net::HTTP.get(url)
+    puts 'end history request'
 
-    histories
+    JSON.parse(res)
   end
 
 end
