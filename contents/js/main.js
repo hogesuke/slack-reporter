312c2277d0d8cf96bc07672d6bc7b2ad0bff1d29
hogesuke <miyado@gmail.com>
2015-12-19 18:04:21 +0900

    jsをちょっとリファクタリング

--- contents/js/main.js
+++ contents/js/main.js
@@ -1,11 +1,14 @@
 $(function() {
   $('a[href^=#]').click(function (e) {
-    var target = $(this.hash);
+    var hash = this.hash;
+    var target = $(hash);
+
     if (!target.length) return;
 
-    var targetY = $("#main-body").scrollTop() + target.offset().top;
-    $('#main-body').animate({ scrollTop: targetY }, 'slow', 'swing', function () {
-      window.location.hash = this.hash
+    var $mainBody = $("#main-body");
+    var targetY = $mainBody.scrollTop() + target.offset().top;
+    $mainBody.animate({ scrollTop: targetY }, 'slow', 'swing', function () {
+      window.location.hash = hash;
     });
 
     return false;
